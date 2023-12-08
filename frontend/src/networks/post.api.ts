c047f8d136478c90c9c6f2f70bd03f43a8b5a964
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-12-08 17:05:30 +0630

    edit post functional

--- frontend/src/networks/post.api.ts
+++ frontend/src/networks/post.api.ts
@@ -2,11 +2,11 @@ import axios from "axios";
 import { Post } from "../models/post.model";
 
 export interface IntialCreateFormProp {
-  imgUrl: string;
-  caption: string;
-  description: string;
-  topic: string[];
-  allowComment: boolean;
+  imgUrl?: string;
+  caption?: string;
+  description?: string;
+  topic?: string[];
+  allowComment?: boolean;
 }
 
 export const CreateAPost = async (
@@ -18,5 +18,15 @@ export const CreateAPost = async (
 
 export const GetUserPosts = async (): Promise<Post[]> => {
   const response = await axios.get("/api/posts/get-user-posts");
+  return response.data;
+};
+
+interface EditFormProps {
+  editForm: IntialCreateFormProp;
+  id?: string;
+}
+
+export const EditMyPost = async ({ editForm, id }: EditFormProps) => {
+  const response = await axios.post(`/api/posts/update-post/${id}`, editForm);
   return response.data;
 };
--- frontend/src/pages/posts/EditPost.tsx
+++ frontend/src/pages/posts/EditPost.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "../../app/store";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Post } from "../../models/post.model";
 import TagTopics from "../../components/CreatePost/TagTopics";
 import "../../styles/checkbox.css";
+import { EditMyPost } from "../../networks/post.api";
+import axios from "axios";
 
 const EditPost = () => {
   const { id } = useParams();
@@ -16,6 +18,9 @@ const EditPost = () => {
     topic: editPost?.topic,
     allowComment: editPost?.allowComment,
   });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const navigate = useNavigate();
 
   useEffect(
     () => setEditPost(createdPosts?.filter((post) => post._id === id)[0]),
@@ -31,8 +36,28 @@ const EditPost = () => {
       }),
     [editPost]
   );
+
+  const handleEdit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(null);
+      await EditMyPost({ editForm, id });
+      setLoading(false);
+      navigate("/profile");
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        setError(error.response?.data.error);
+      }
+      setLoading(false);
+    }
+  };
+
   return (
-    <main className="mt-10 md:mt-24 mb-28 max-w-[700px] mx-auto px-3">
+    <form
+      onSubmit={handleEdit}
+      className="mt-10 md:mt-24 mb-28 max-w-[700px] mx-auto px-3"
+    >
       <img
         src={editPost?.imgUrl}
         alt={editPost?.caption}
@@ -44,6 +69,9 @@ const EditPost = () => {
           type="text"
           id="title"
           defaultValue={editForm?.caption}
+          onChange={(e) =>
+            setEditForm({ ...editForm, caption: e.target.value })
+          }
           className="border-none outline-none block px-3 py-5 w-full bg-transparent caret-[var(--pri-red)]"
           placeholder="Tell everyone what your post is about"
         />
@@ -54,6 +82,9 @@ const EditPost = () => {
           type="text"
           id="description"
           defaultValue={editForm?.description}
+          onChange={(e) =>
+            setEditForm({ ...editForm, description: e.target.value })
+          }
           className="border-none outline-none block px-3 py-5 w-full bg-transparent caret-[var(--pri-red)]"
           placeholder="Add a description to your post"
         />
@@ -78,6 +109,7 @@ const EditPost = () => {
         </label>
         <span>Allow comments</span>
       </div>
+      {error && <p className="text-center text-red-500 text-[17px]">{error}</p>}
       <div className="flex items-center justify-end gap-3 px-4 my-6">
         <Link
           className="py-3 px-5 rounded-full bg-[var(--sec-light)] hover:bg-gray-300 self-end"
@@ -85,11 +117,14 @@ const EditPost = () => {
         >
           Cancel
         </Link>
-        <button className="py-3 px-5 rounded-full bg-[var(--pri-red)] hover:bg-[var(--sec-red)] text-white self-end">
-          Save
+        <button
+          disabled={loading}
+          className="py-3 px-5 rounded-full bg-[var(--pri-red)] hover:bg-[var(--sec-red)] disabled:opacity-50 text-white self-end"
+        >
+          {loading ? "Saving " : "Save"}
         </button>
       </div>
-    </main>
+    </form>
   );
 };
 
