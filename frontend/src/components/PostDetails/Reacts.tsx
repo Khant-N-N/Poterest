313a5b76c638863ca1bae42cb9a2577cf88a8cd9
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-12-14 01:57:14 +0630

    change position of love and good, add title

--- frontend/src/components/PostDetails/Reacts.tsx
+++ frontend/src/components/PostDetails/Reacts.tsx
@@ -42,46 +42,62 @@ const Reacts = ({ postData }: ReactionsProps) => {
       <div>2</div>
       <div className="group">
         <div className="hidden group-hover:flex md:group-hover:-top-20 gap-3 bg-[var(--light)] shadow-2xl p-3 rounded-full absolute z-30 right-0 -top-14">
-          <img
-            onClick={giveReaction}
-            title="good idea"
-            id="good_idea"
-            src={good_idea}
-            alt="good react"
-            className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
-          />
-          <img
-            onClick={giveReaction}
-            title="love"
-            id="love"
-            src={love}
-            alt="love react"
-            className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
-          />
-          <img
-            onClick={giveReaction}
-            title="thanks"
-            id="thanks"
-            src={thanks}
-            alt="thanks react"
-            className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
-          />
-          <img
-            onClick={giveReaction}
-            title="wow"
-            id="wow"
-            src={wow}
-            alt="wow react"
-            className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
-          />
-          <img
-            onClick={giveReaction}
-            title="haha"
-            id="haha"
-            src={haha}
-            alt="haha react"
-            className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
-          />
+          <div className="flex flex-col items-center">
+            <img
+              onClick={giveReaction}
+              title="love"
+              id="love"
+              src={love}
+              alt="love react"
+              className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
+            />
+            <span className="text-[13px]">Love</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              onClick={giveReaction}
+              title="good idea"
+              id="good_idea"
+              src={good_idea}
+              alt="good react"
+              className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
+            />
+            <span className="text-[13px]">Good idea</span>
+          </div>
+
+          <div className="flex flex-col items-center">
+            <img
+              onClick={giveReaction}
+              title="thanks"
+              id="thanks"
+              src={thanks}
+              alt="thanks react"
+              className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
+            />
+            <span className="text-[13px]">Thanks</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              onClick={giveReaction}
+              title="wow"
+              id="wow"
+              src={wow}
+              alt="wow react"
+              className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
+            />
+            <span className="text-[13px]">Wow</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              onClick={giveReaction}
+              title="haha"
+              id="haha"
+              src={haha}
+              alt="haha react"
+              className="w-10 h-10 md:w-14 md:h-14 hover:scale-[1.3] hover:-translate-y-4 cursor-pointer transition-transform"
+            />
+            <span className="text-[13px]">Haha</span>
+          </div>
         </div>
         <div
           onClick={() => {
