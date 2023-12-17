9792d76b6f8593b4b3fe01ebbc4b8775ff0cebdc
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-12-17 17:11:06 +0630

    err msg and loading button

--- frontend/src/components/PostDetails/AddComment.tsx
+++ frontend/src/components/PostDetails/AddComment.tsx
@@ -41,32 +41,42 @@ const AddComment = ({ postData }: AddCommentProps) => {
     }
   };
   return (
-    <form
-      onSubmit={handleAddComment}
-      className="flex gap-2 items-center relative"
-    >
-      <img
-        src={logInUser?.avatar}
-        alt="my profile"
-        className="w-10 h-10 md:w-14 md:h-14 rounded-full object-cover"
-      />
-      {postData?.allowComment ? (
-        <input
-          value={commentText}
-          onChange={(e) => setCommentText(e.target.value)}
-          type="text"
-          placeholder="Add a comment"
-          className="p-4 bg-[var(--sec-light)] focus:bg-transparent focus:border-2 outline-none rounded-full w-full"
-        />
-      ) : (
-        <p className="text-gray-400 text-[15px] md:text-[19px]">
-          This comment section is disabled by the owner
+    <>
+      {error && (
+        <p className="text-center text-red-500 text-[16px]">
+          Something went wrong! please try again.
         </p>
       )}
-      <button className="absolute right-4 bg-[var(--pri-red)] p-2 rounded-full text-white">
-        <IoSend />
-      </button>
-    </form>
+      <form
+        onSubmit={handleAddComment}
+        className="flex gap-2 items-center relative"
+      >
+        <img
+          src={logInUser?.avatar}
+          alt="my profile"
+          className="w-10 h-10 md:w-14 md:h-14 rounded-full object-cover"
+        />
+        {postData?.allowComment ? (
+          <input
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+            type="text"
+            placeholder="Add a comment"
+            className="p-4 bg-[var(--sec-light)] focus:bg-transparent focus:border-2 outline-none rounded-full w-full"
+          />
+        ) : (
+          <p className="text-gray-400 text-[15px] md:text-[19px]">
+            This comment section is disabled by the owner
+          </p>
+        )}
+        <button
+          disabled={loading}
+          className="absolute disabled:opacity-40 right-4 bg-[var(--pri-red)] p-2 rounded-full text-white"
+        >
+          <IoSend />
+        </button>
+      </form>
+    </>
   );
 };
 
